dfb3a3b74c02de4d83ed311629e72f15ab29aa91
polizz <polizz@nowhere>
2024-04-07 23:15:53 -0500

    Remove static file list

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,131 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use std::{borrow::BorrowMut, path::PathBuf};
+use anyhow::Result;
+use crossterm::terminal::ClearType;
+use std::ffi::OsString;
+use std::io::{stdout, Write};
+use std::path::PathBuf;
 
-use inquire::{InquireError, Select};
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    queue, style, terminal,
+};
 
-// fn set_picked_scheme(input: &str, option: &&str, string_value: &str, _idx: usize) -> Option<i64> {
-//     println!(
-//         "Current value: input: {:?}, option: {:?}, string_value: {:?}",
-//         input, option, string_value
-//     );
-//
-//     Some(1i64)
-// }
-fn main() {
-    let config_dir = PathBuf::from("~/.config/alacritty/");
+fn write_config(
+    alacritty_config_base: &PathBuf,
+    scheme_file: &PathBuf,
+    config_out_file: &PathBuf,
+) -> Result<()> {
+    let mut base_content = std::fs::read_to_string(alacritty_config_base)?;
+    let theme_content = std::fs::read_to_string(scheme_file)?;
+    base_content.push_str(&theme_content);
 
-    let options: Vec<&str> = vec![
-        "_default.yml",
-        "ayu-dark.yml",
-        "ayu-light.yml",
-        "ayu-mirage.yml",
-        "catppuccin-frappe.yml",
-        "catppuccin-latte.yml",
-        "catppuccin-macchiato.yml",
-        "catppuccin-mocha.yml",
-        "dracula.yml",
-        "firewatch-light.yml",
-        "firewatch.yml",
-        "mountaineer-grey.yml",
-        "mountaineer-light.yml",
-        "mountaineer.yml",
-        "night-owl.yml",
-        "nord.yml",
-        "papercolor-light.yml",
-        "pink-moon.yml",
-        "seoul256-light.yml",
-        "seoul256.yml",
-    ];
+    std::fs::write(config_out_file, base_content)?;
 
-    let select_history: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(vec![]));
-
-    let cloned_select_history = select_history.clone();
-    let set_picked_scheme =
-        |_input: &str, _option: &&str, string_value: &str, _idx: usize| -> Option<i64> {
-            // println!(
-            //     "Current value: input: {:?}, option: {:?}, string_value: {:?}",
-            //     input, option, string_value
-            // );
-            let mut cloned_select_history = (*cloned_select_history).borrow_mut();
-            cloned_select_history.push(string_value.to_owned());
-
-            Some(1i64)
-        };
-
-    let scheme: Result<&str, InquireError> =
-        Select::new("Select a color scheme and enter when finished:", options)
-            .with_scorer(&set_picked_scheme)
-            .with_vim_mode(true)
-            .prompt();
-
-    match scheme {
-        Ok(choice) => println!("{}! That's mine too!", choice),
-        Err(_) => println!("There was an error, please try again"),
-    }
-
-    println!("Select history: {:#?}", (*select_history).borrow_mut());
+    Ok(())
 }
 
-// config_dir=~/.config
-// alacritty_config=${config_dir}/alacritty
-// alacritty_themes=${alacritty_config}/themes
-// curr_color_file=${alacritty_config}/curr_color
-//
-// declare -a arr=($(ls -al ${alacritty_themes} | awk 'NR>3 { printf("%s ", $9) }'))
-//
-// curr_color=$(cat ${curr_color_file} 2>/dev/null)
-//
-// if [[ -z "$curr_color" ]]; then
-//    next_color=${arr[0]}
-// else
-//    len=${#arr[@]}
-//    for ((i = 0; i < len; i++));
-//    do
-//       # echo "Checking: ${arr[$i]} against: ${curr_color}"
-//       if [ "${arr[$i]}" == "$curr_color" ]; then
-//          if [ $((i+1)) -ge "$len" ]; then
-//             next_color="${arr[0]}"
-//          else
-//             next_color="${arr[$i+1]}"
-//          fi
-//
-//          break
-//       fi
-//    done
-// fi
-//
-// $(echo ${next_color} > "${curr_color_file}")
-// echo "Picking next color: ${next_color}"
-//
-// cat ~/.config/alacritty/base.yml ~/.config/alacritty/themes/${next_color} > ~/.config/alacritty/alacritty.yml
+fn read_current(curr_file_path: &PathBuf) -> Result<String> {
+    let current_theme = std::fs::read_to_string(curr_file_path).unwrap_or("".to_owned());
+    Ok(current_theme)
+}
+
+fn get_color_schemes(dir: &PathBuf) -> Result<Vec<OsString>> {
+    let contents = dir.read_dir()?;
+
+    Ok(contents
+        .filter_map(|en| {
+            if en.is_ok() {
+                Some(en.unwrap().file_name())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<OsString>>())
+}
+
+fn main() -> Result<()> {
+    let mut base_config = PathBuf::from("/Users/polizz/.config/alacritty/");
+
+    let mut themes_dir = base_config.clone();
+    let mut alacritty_config = base_config.clone();
+    let mut curr_theme = base_config.clone();
+
+    base_config.push("base.yml");
+    themes_dir.push("themes");
+    alacritty_config.push("alacritty.yml");
+    curr_theme.push("curr_color");
+
+    let current_theme = read_current(&curr_theme)?;
+
+    let mut options = get_color_schemes(&themes_dir)?;
+    options.sort();
+
+    terminal::enable_raw_mode()?;
+    let mut stdout = stdout();
+    queue!(stdout, cursor::MoveToNextLine(1))?;
+
+    let current_theme_msg = format!("Current theme: {}", &current_theme);
+    queue!(
+        stdout,
+        style::Print(current_theme_msg),
+        cursor::MoveToNextLine(0),
+        style::Print("Press [n] to cycle themes, [q] to exit, [enter] to accept"),
+        cursor::MoveToNextLine(0),
+        style::Print("\r\n")
+    )?;
+    stdout.flush()?;
+
+    let mut themes = options.iter().cycle();
+    loop {
+        let char = read()?;
+
+        match char {
+            Event::Key(key_event) => match key_event.code {
+                KeyCode::Enter => {
+                    queue!(
+                        stdout,
+                        style::Print("Saving theme"),
+                        cursor::MoveToNextLine(1),
+                    )?;
+
+                    break;
+                }
+                KeyCode::Char(code) => match code {
+                    'q' => {
+                        let trimmed_theme = current_theme.trim();
+                        themes_dir.push(trimmed_theme);
+                        queue!(
+                            stdout,
+                            style::Print("Reverting to "),
+                            style::Print(trimmed_theme),
+                            cursor::MoveToNextLine(1),
+                        )?;
+                        write_config(&base_config, &themes_dir, &alacritty_config)?;
+                        break;
+                    }
+                    'n' => {
+                        let theme = themes.next().unwrap().to_str().unwrap();
+                        themes_dir.push(theme);
+                        write_config(&base_config, &themes_dir, &alacritty_config)?;
+                        themes_dir.pop();
+                        queue!(
+                            stdout,
+                            terminal::Clear(ClearType::CurrentLine),
+                            style::Print(theme),
+                            cursor::MoveToColumn(0)
+                        )?;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
+        }
+
+        stdout.flush()?;
+    }
+
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
